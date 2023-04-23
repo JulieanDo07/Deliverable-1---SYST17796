ee6fd3046ef4eea9d4dfe580c7fa31c027a8cad9
aamberdo0 <122891092+aamberdo0@users.noreply.github.com>
2023-04-23 00:23:02 -0400

    Update HumanPlayer.java

--- HumanPlayer.java
+++ HumanPlayer.java
@@ -1,35 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package softwareproj;
-
-import java.util.ArrayList;
-
 /**
  *
- * @author julieando
- *
- * @author Amber Do
- * 
+ * @author Sattar, Farzana
+ * @author Do, Juliean
+ * @author Do, Amber
+ * Submission Date: April 22, 2023
  */
+package softwareproj;
+import java.util.ArrayList;
 public class HumanPlayer extends Player{
     private ArrayList<CardHand> humanCard; 
     private int humanScore; 
-    
-    
     public HumanPlayer(String name){
         super(name);
         humanCard = new ArrayList<>(); 
         this.humanScore = humanScore; 
-    }
-    
+    } 
     public void play(){
-        
     }
-    
-    
-    
-    
-    
 }
