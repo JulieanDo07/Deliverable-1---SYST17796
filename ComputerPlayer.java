6419e32b1925c78417a9231c0190481b6a096766
aamberdo0 <122891092+aamberdo0@users.noreply.github.com>
2023-04-23 00:15:44 -0400

    Update ComputerPlayer.java

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -1,17 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package softwareproj;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author julieando
- * @author Amber Do
- * 
- */
 public class ComputerPlayer extends Player{
     private ArrayList<CardHand> compCard; 
     private int compScore; 
